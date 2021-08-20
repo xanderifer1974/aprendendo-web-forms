f9c5e6175c03b805f909a3490bb8a8084ee0f8e1
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-08-19 22:21:02 -0300

    Aula 11 - Table e placeholder

--- WaConhecendoComponentes/wfTabuada.aspx
+++ WaConhecendoComponentes/wfTabuada.aspx
@@ -5,9 +5,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 1738px;
+            height: 988px;
+        }
+    </style>
 </head>
 <body style="width: 1740px; height: 898px">
-     <form id="form1" runat="server">
+     <form id="form1" runat="server" class="auto-style1">
         <div>
             <h1>Tabuada</h1>
             <h1>
@@ -17,7 +23,7 @@
             </h1>
             <asp:ListBox ID="lbDados" runat="server" Height="374px" Width="167px" Visible="False"></asp:ListBox>
         </div>
-         <asp:Table ID="TbDados" runat="server" BackColor="#FFFFCC" BorderColor="Maroon" BorderStyle="Solid" BorderWidth="2px" CellPadding="5" Height="384px" Width="353px">
+         <asp:Table ID="TbDados" runat="server" BackColor="#FFFFCC" BorderColor="Maroon" BorderStyle="Solid" BorderWidth="2px" CellPadding="5" Height="384px" Width="353px" Visible="False">
              <asp:TableRow runat="server">
                  <asp:TableCell runat="server"></asp:TableCell>
                  <asp:TableCell runat="server">x</asp:TableCell>
@@ -97,6 +103,7 @@
              </asp:TableRow>     
             
          </asp:Table>
+         <asp:PlaceHolder ID="PlaceHolder" runat="server"></asp:PlaceHolder>
     </form>
 </body>
 </html>
--- WaConhecendoComponentes/wfTabuada.aspx.cs
+++ WaConhecendoComponentes/wfTabuada.aspx.cs
@@ -26,14 +26,32 @@ namespace WaConhecendoComponentes
             //lbDados.Items.Clear();
             ListItem li = DlNumeros.SelectedItem;
             int n = Convert.ToInt32(li.Value);
+           
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    t = i * n;
+            //    //lbDados.Items.Add(i.ToString() + "x" + n.ToString() + " = " + t.ToString());
+            //    TbDados.Rows[i].Cells[0].Text = n.ToString();
+            //    TbDados.Rows[i].Cells[4].Text = t.ToString();
+            //}
+
+            Table tabela = new Table();
             int t = 0;
-            for (int i = 0; i < 11; i++)
+            for (int i=0; i<=10; i++)
             {
+                TableRow linha = new TableRow();
+                //1 celula
+                TableCell coluna = new TableCell();
+                coluna.Text = n.ToString() + " x " + i.ToString() + " = ";
+                linha.Cells.Add(coluna);
+                //2 celula
+                coluna = new TableCell();
                 t = i * n;
-                //lbDados.Items.Add(i.ToString() + "x" + n.ToString() + " = " + t.ToString());
-                TbDados.Rows[i].Cells[0].Text = n.ToString();
-                TbDados.Rows[i].Cells[4].Text = t.ToString();
+                coluna.Text = t.ToString();
+                linha.Cells.Add(coluna);
+                tabela.Rows.Add(linha);
             }
+            PlaceHolder.Controls.Add(tabela);
 
         }
 
